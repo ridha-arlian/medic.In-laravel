59e4910f3c108a2690801d919ff251e73957a07d
ridha-arlian <ridhaarlian@proton.me>
2025-10-14 03:27:40 +0700

    feat(Specialization): add auto generate prefix ID

--- app/Models/Specialization.php
+++ app/Models/Specialization.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Omaressaouaf\LaravelIdGenerator\IdGenerator;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Specialization extends Model
 {
-    protected $fillable = ['name', 'description', 'is_active'];
+    protected $fillable = ['name', 'description', 'code', 'is_active'];
 
     protected $casts = [
         'is_active' => 'boolean',
@@ -16,5 +17,19 @@ class Specialization extends Model
     public function dokter(): HasOne
     {
         return $this->hasOne(Dokter::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($model) {
+            $model->code = IdGenerator::generate(
+                self::class,
+                'code',
+                5,
+                'SP-'
+            );
+        });
     }
 }
